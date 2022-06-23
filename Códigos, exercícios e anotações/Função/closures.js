e6b67f7687b5d9169ae8618b1d79af9033baef6a
cainamicael <cainamicaeloficial@gmail.com>
2022-06-23 11:00:35 -0300

    add

---
+++ Códigos, exercícios e anotações/Função/closures.js
@@ -0,0 +1,17 @@
+//É o escopo criado quando uma função é declarada
+//Esse escopo permite a função acerrar e manipular variáveis externas à função
+
+//Contexto léxico em ação
+
+const x = 'Global'
+
+function fora(){
+    const x = 'Local'
+    function dentro(){
+        return x
+    }
+    return dentro
+}
+
+const minhaFuncao = fora()
+console.log(minhaFuncao())
---
+++ Códigos, exercícios e anotações/Função/tiposDeclaração.js
@@ -0,0 +1,14 @@
+console.log(soma(3, 4))
+// console.log(sub(3, 4)) dá erro
+//console.log(mult(3,4)) tb dá erro
+
+//function declaration
+function soma(x, y){
+    return x + y
+}
+
+//function expression
+const sub = function (x, y){
+    return x - y
+}
+
