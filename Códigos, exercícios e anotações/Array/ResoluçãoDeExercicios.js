6ed30f8979acea1b83290a5683c61bdb5a05f7fc
cainamicael <cainamicaeloficial@gmail.com>
2022-06-29 17:00:03 -0300

    a

--- Códigos, exercícios e anotações/Array/ResoluçãoDeExercicios.js
+++ Códigos, exercícios e anotações/Array/ResoluçãoDeExercicios.js
@@ -47,3 +47,48 @@ console.log(inverso("Programação"))
 
 //Questão 7
 
+//Questão 8
+function multiplicar(n1, n2){
+    let m = 0
+    let n = 'Digite números inteiros'
+    if (n1 >= 0 && n2 >= 0){
+        for(i = 0; i < n2; i++){
+            m += n1
+        }
+        return m
+    }else {
+        return n 
+    }
+}
+console.log(multiplicar(5,2), multiplicar(20, 4), multiplicar(10, 100))
+console.log(multiplicar(5,-2), multiplicar(-20, 4), multiplicar(-10, -100))
+
+//Questão 9
+const repetir = (parametro, qtd) => {
+    const newArray = []
+    for (let i = 0; i < qtd; i++){
+        newArray.push(parametro)
+    }
+    return newArray
+}
+// ou
+const repetir2 = (item, qtd) => {
+    return Array(qtd).fill(item)
+}
+
+console.log(repetir('Código', 8))
+console.log(repetir2(7, 3))
+console.log(repetir2('Cainã', 15))
+
+//Questão 10
+function simboloMais(vezes){
+    let res = ''
+    for (i=0; i<vezes; i++){
+        res += '+'
+    }
+    return res
+}
+console.log(simboloMais(2))
+console.log(simboloMais(20))
+console.log(simboloMais(1))
+console.log(simboloMais(40))
